3ab7d6c0c2b3dfb1b7f238b7fa421e23daedb492
Donadeveloper <luca.donadel05@gmail.com>
2022-08-16 22:06:37 +0200

    added submit button

--- src/components/AddTravelForm.js
+++ src/components/AddTravelForm.js
@@ -1,19 +1,28 @@
 import React from 'react'
+import { useState } from "react";
 
 const AddTravelForm = () => {
+
+  const [date, setDate] = useState();
+
+  const submit = () => {
+      console.log(date);
+  }
+
   return (
     <div id='addTravel'>
         <form className='addTravelForm'>
             <label>From:</label><br />
-            <input id='formFrom' type='text'></input><br />
+            <input id='formFrom' type='text' /><br />
             <label>To:</label><br />
-            <input id='formTo' type='text'></input><br />
+            <input id='formTo' type='text' /><br />
             <label>Distance Travelled:</label><br />
-            <input id='formDistanceTravelled' type='number'></input><br />
+            <input id='formDistanceTravelled' type='number' /> Km<br />
             <label>Car Used:</label><br />
-            <input id='formCarUsed' type='text'></input><br />
+            <input id='formCarUsed' type='text' /><br />
             <label>Date:</label><br />
-            <input id='formDate' type='date'></input>
+            <input id='formDate' type='date' onChange={(event) => setDate(event.target.value)}/>
+            <input className='submitButton' defaultValue='&#10003; Submit' onSubmit={submit()}/>
         </form>
     </div>
   )
