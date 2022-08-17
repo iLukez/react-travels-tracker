adc1d8d04a05bcba7325a8d48248ffac039fefaa
Donadeveloper <luca.donadel05@gmail.com>
2022-08-17 19:59:07 +0200

    Fixed a bug when submitting the form

--- src/components/AddTravelForm.js
+++ src/components/AddTravelForm.js
@@ -3,26 +3,39 @@ import { useState } from "react";
 
 const AddTravelForm = () => {
 
-  const [date, setDate] = useState();
+  // States for the data of the new travel
+
+  const [from, setFrom] = useState('/');
+  const [to, setTo] = useState('/');
+  const [distance, setDistance] = useState('/');
+  const [car, setCar] = useState('/');
+  const [date, setDate] = useState('1/1/1960');
+
+  // Test submit function, todo: remove
 
   const submit = () => {
-      console.log(date);
+    console.log(from);
+    console.log(to);
+    console.log(distance);
+    console.log(car);
+    console.log(date);
   }
+
 
   return (
     <div id='addTravel'>
-        <form className='addTravelForm'>
+        <form className='addTravelForm' >
             <label>From:</label><br />
-            <input id='formFrom' type='text' /><br />
+            <input id='formFrom' type='text' onChange={(event) => setFrom(event.target.value)} /><br />
             <label>To:</label><br />
-            <input id='formTo' type='text' /><br />
+            <input id='formTo' type='text' onChange={(event) => setTo(event.target.value)} /><br />
             <label>Distance Travelled:</label><br />
-            <input id='formDistanceTravelled' type='number' /> Km<br />
+            <input id='formDistanceTravelled' type='number' onChange={(event) => setDistance(event.target.value)} /> Km<br />
             <label>Car Used:</label><br />
-            <input id='formCarUsed' type='text' /><br />
+            <input id='formCarUsed' type='text' onChange={(event) => setCar(event.target.value)} /><br />
             <label>Date:</label><br />
-            <input id='formDate' type='date' onChange={(event) => setDate(event.target.value)}/>
-            <input className='submitButton' defaultValue='&#10003; Submit' onSubmit={submit()}/>
+            <input id='formDate' type='date' onChange={(event) => setDate(event.target.value)} /><br />
+            <button type='button' className='submitButton' onClick={() => submit()}>&#10003; Submit</button>
         </form>
     </div>
   )
