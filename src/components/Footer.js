43f5f475da9a9dfda51ed4d4b4d82279142a2221
iLukez <luca.donadel05@gmail.com>
2022-09-18 21:35:46 +0200

    Updated footer, updated navbar style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>Footer TODO</p>
+      <p>Donadel Luca, 30/06/2022</p>
+      <p>Webpage made with React</p>
+      <a href='https://github.com/iLukez/react-travels-tracker' target={'_blank'} rel={'noopener noreferrer'}>GITHUB</a>
     </div>
   )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,20 +4,12 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <nav className='navbar'>
-        <Link to={'/'}  className='nav-element' style={{textDecoration: 'none', color: 'rgb(20, 20, 200)'}}>
+        <Link to={'/'}  className='nav-element' style={{borderRight: '2px solid rgb(226, 226, 226)'}}>
             <strong>Travels List</strong>
         </Link>
 
-        <Link to={'/cars'}  className='nav-element' style={{textDecoration: 'none', color: 'rgb(20, 20, 200)'}}>
+        <Link to={'/cars'}  className='nav-element'>
             <strong>My Cars</strong>
-        </Link>
-
-        <Link to={'/'}  className='nav-element' style={{textDecoration: 'none', color: 'rgb(20, 20, 200)'}}>
-            <strong>Expenses Details TODO</strong>
-        </Link>
-        
-        <Link to={'/'}  className='nav-element' style={{textDecoration: 'none', color: 'rgb(20, 20, 200)'}}>
-            <strong>Function TODO</strong>
         </Link>
     </nav>
   )
